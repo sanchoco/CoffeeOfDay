c4f80e4f4aeaa63031a1e3bbcd20a8d2cfd13ba8
sanchoco <jguuun@gmail.com>
2021-03-03 22:26:16 +0900

    닉네임 2글자 이상 입력시에 처리

--- static/base.js
+++ static/base.js
@@ -26,31 +26,31 @@ function DislikeCoffee(name) {
 
 // 글쓰기
 function WriteComment(comment, number, nickname) {
-    if (comment=='') {
+    if (comment == '') {
         alert("내용을 입력해주세요.")
         window.location.reload()
         return
-    } else if (!number || number== '' || !nickname || nickname == '') {
+    } else if (!number || number == '' || !nickname || nickname == '') {
         alert("문제가 발생하였습니다. 다시 시도해주세요.")
         window.location.reload()
         return
     }
     $.ajax({
-            type: 'POST',
-            url: '/api/write',
-            data: {comment: comment, number: number, nickname: nickname},
-            success:function (response) {
-                if (response['msg'] == 'error'){
-                    alert("에러가 발생하였습니다. 다시 시도해주세요.")
-                } else {
-                    alert(response['msg'])
-                }
-                window.location.reload()
-            },
-            error:function (error){
-                alert("다시 시도해주세요.")
-                window.location.reload()
+        type: 'POST',
+        url: '/api/write',
+        data: {comment: comment, number: number, nickname: nickname},
+        success: function (response) {
+            if (response['msg'] == 'error') {
+                alert("에러가 발생하였습니다. 다시 시도해주세요.")
+            } else {
+                alert(response['msg'])
             }
+            window.location.reload()
+        },
+        error: function (error) {
+            alert("다시 시도해주세요.")
+            window.location.reload()
+        }
     })
 }
 
@@ -59,4 +59,145 @@ function deleteToken() {
     $.removeCookie('mytoken', {path: '/'});
     alert('로그아웃 완료!')
     window.location.reload()
+}
+
+function register() {
+    let username = $("#input-username").val()
+    let password = $("#input-password").val()
+    let nickname = $("#input-nickname").val()
+    let password2 = $("#input-password2").val()
+
+
+    if ($("#help-id").hasClass("is-danger")) {
+        alert("아이디를 다시 확인해주세요.")
+        return;
+    } else if (!$("#help-id").hasClass("is-success")) {
+        alert("아이디 중복확인을 해주세요.")
+        return;
+    }
+    if ($("#help-nick").hasClass("is-danger")) {
+        alert("닉네임 중복확인을 해주세요.")
+        return;
+    }
+
+    if (password == "") {
+        $("#help-password").text("비밀번호를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+
+        $("#input-password").focus()
+        return;
+    } else if (!is_password(password)) {
+        $("#help-password").text("비밀번호의 형식을 확인해주세요. 영문과 숫자 필수 포함, 특수문자(!@#$%^&*) 사용가능 8-20자").removeClass("is-safe").addClass("is-danger")
+        $("#input-password").focus()
+        return
+    } else {
+        $("#help-password").text("사용할 수 있는 비밀번호입니다.").removeClass("is-danger").addClass("is-success")
+    }
+    if (nickname == "") {
+        $("#help-nickname").text("닉네임을 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+        $("#input-nickname").focus()
+        return
+    } else {
+        $("#help-nickname").text("멋진 닉네임이네요!").removeClass("is-danger").addClass("is-success")
+    }
+    if (password2 == "") {
+        $("#help-password2").text("비밀번호를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+        $("#input-password2").focus()
+        return;
+    } else if (password2 != password) {
+        $("#help-password2").text("비밀번호가 일치하지 않습니다.").removeClass("is-safe").addClass("is-danger")
+        $("#input-password2").focus()
+        return;
+    } else {
+        $("#help-password2").text("비밀번호가 일치합니다.").removeClass("is-danger").addClass("is-success")
+    }
+    $.ajax({
+        type: "POST",
+        url: "api/sign_up",
+        data: {
+            username_give: username,
+            password_give: password,
+            nickname_give: nickname
+        },
+        success: function (response) {
+            alert("회원가입을 축하드립니다!")
+            window.location.replace("/login")
+        }
+    });
+
+}
+
+function is_id(asValue) {
+    var regExp = /^(?=.*[a-zA-Z])[-a-zA-Z0-9_.]{2,10}$/;
+    return regExp.test(asValue);
+}
+
+function is_password(asValue) {
+    var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,20}$/;
+    return regExp.test(asValue);
+}
+
+function check_nick() {
+    let nickname = $("#input-nickname").val()
+    if (nickname.length < 2) {
+        $("#help-nick").text("닉네임을 입력해주세요").removeClass("is-safe").addClass("is-danger")
+        return
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/api/check_nick",
+        data: {
+            nickname_give: nickname
+        },
+        success: function (response) {
+            if (response["nickexists"]) {
+                $("#help-nick").text("이미 존재하는 닉네임입니다.").removeClass("is-safe").addClass("is-danger")
+                $("#input-nickname").focus()
+            } else {
+                $("#help-nick").text("멋진 닉네임입니다!").removeClass("is-danger").addClass("is-success")
+            }
+
+
+        }
+    });
+
+
+}
+
+
+function check_dup() {
+    let username = $("#input-username").val()
+    if (username == "") {
+        $("#help-id").text("아이디를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+        $("#input-username").focus()
+        return;
+    }
+    if (!is_id(username)) {
+        $("#help-id").text("아이디의 형식을 확인해주세요. 영문과 숫자, 일부 특수문자(._-) 사용 가능. 2-10자 길이").removeClass("is-safe").addClass("is-danger")
+        $("#input-username").focus()
+        return;
+    }
+    $("#help-id").addClass("is-loading")
+
+    $.ajax({
+        type: "POST",
+        url: "/api/check_dup",
+        data: {
+            username_give: username
+        },
+        success: function (response) {
+            if (response["exists"]) {
+                $("#help-id").text("이미 존재하는 아이디입니다.").removeClass("is-safe").addClass("is-danger")
+                $("#input-username").focus()
+            } else {
+                $("#help-id").text("사용할 수 있는 아이디입니다.").removeClass("is-danger").addClass("is-success")
+            }
+            $("#help-id").removeClass("is-loading")
+
+        }
+    });
+}
+
+function cancel() {
+    window.location.href = "/login"
 }
